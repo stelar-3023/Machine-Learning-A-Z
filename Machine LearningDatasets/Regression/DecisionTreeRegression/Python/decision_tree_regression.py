5eeab51397c8c6ef1b2ff311a4b45670b5f555e4
Steve Larsen <slarsen-3@att.net>
2020-10-04 09:46:15 -0500

    updated RandomForestRegression

--- Machine LearningDatasets/Regression/DecisionTreeRegression/Python/decision_tree_regression.py
+++ Machine LearningDatasets/Regression/DecisionTreeRegression/Python/decision_tree_regression.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
+dataset = pd.read_csv("E:\MachineLearning\MachineLearningDatasets\Regression\DecisionTreeRegression\Python\Position_Salaries.csv")
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
